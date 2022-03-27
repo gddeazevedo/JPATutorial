b92a59dbbbee067024a21b06dced51789f1f1474
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2022-03-27 15:19:15 -0300

    adding entity

--- src/main/java/com/example/jpaTutorial/entities/Student.java
+++ src/main/java/com/example/jpaTutorial/entities/Student.java
@@ -1,4 +1,54 @@
-package com.example.jpaTutorial.models;
+package com.example.jpaTutorial.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "Student")
+@Table(
+        name = "students",
+        uniqueConstraints = @UniqueConstraint(
+                name = "email",
+                columnNames = "email"
+        )
+)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name="students_sequence",
+            sequenceName = "students_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "students_sequence"
+    )
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "guardian_name")
+    private String guardianName;
+
+    @Column(name = "guardian_email")
+    private String guardianEmail;
+
+    @Column(name = "guardian_mobile")
+    private String guardianMobile;
+
 }
